72070bf00ddffc290a71b987373d3ea2455cf3cf
Tom <tomas.walshy@gmail.com>
2018-06-14 13:06:43 +1000

    Added front end and functionality

--- cocktails.js
+++ cocktails.js
@@ -6,27 +6,78 @@ $(function(){
   var ingredient3 = '';
   var ingredient4 = '';
 
+  var drink = '';
 
+  //Gets Json data from database at https://www.thecocktaildb.com
   function getCocktails(){
-    console.log('cocktail function run');
-      //Gets Json data from database at https://www.thecocktaildb.com
+    $('.drinkName').text('Loading cocktail...');
+    $('.drinkDeets').text('');
+    $('.drinkIngr').text('');
+    var url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredient1;
+
+    //create url with ingredients as filters
+    if (ingredient2 != '') {
+      url += '&&i=' + ingredient2;
+      if (ingredient3 != '') {
+        url += '&&i=' + ingredient3;
+        if (ingredient4 != '') {
+          url += '&&i=' + ingredient4;
+        }
+      }
+    }
+    console.log(url);
+    //url of specific drink selected
+    var specUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+    //Get random cocktail containing ingredients
+    $.getJSON( url , function(data){
+        let drinknum = data.drinks.length;
+        drink = Math.floor(Math.random()*drinknum);
+        specUrl += encodeURIComponent(data.drinks[drink].strDrink.trim());
+
+    //Get details of the random cocktail and display
+        $.getJSON(specUrl, function(data){
+          let drinkObj = data.drinks[0];
+          console.log(drinkObj);
+          $('.drinkName').text(drinkObj.strDrink);
+          $('.drinkIngr').text(drinkObj.strIngredient1 + ', ' + drinkObj.strIngredient2 + ', ' + drinkObj.strIngredient3 + ', ' + drinkObj.strIngredient4);
+          $('.drinkDeets').text(drinkObj.strInstructions);
+        });
+
+    });
+
+    //API requests using vanilla XML JavaScript code
+    /*
       var request = new XMLHttpRequest();
-      //var url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredient1;
-      request.open('GET', 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i='+ingredient1 + '&&i=' + ingredient2 + '&&i=' + ingredient3 + '&&i=' + ingredient4);
+      var url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+      //add ingredient filters to api request
+      request.open('GET', url +ingredient1 + '&&i=' + ingredient2 + '&&i=' + ingredient3 + '&&i=' + ingredient4);
       request.onload = function(){
         var Responseobject = JSON.parse(request.responseText);
+        //returns a random drink from the possible results
         let drinknum = Responseobject.drinks.length;
-        let randDrink = Math.floor(Math.random()*drinknum);
+        drink = Math.floor(Math.random()*drinknum);
         console.log(randDrink);
         $('.bodyText').text(Responseobject.drinks[randDrink].strDrink);
 
+
+        //return details of drink from seperate api
+        var drinkRequest = new XMLHttpRequest();
+        request.open('GET', 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + encodeURIComponent(drink.trim()));
+        request.onload = function(){
+          var responseObj = JSON.parse(drinkRequest.responseText);
+          $('.drinkDeets').text(responseObj.drinks.strInstructions);
+        }
+
       }
       request.send();
+      drinkRequest.send();
+      */
   }
 
-  //Check for submitted Ingredients
+  //Update ingredient variables
   $('#cocktailButton').click(function(){
-    //update ingredient variable
+    //update ingredient variable and escape spaces for url
     ingredient1 = encodeURIComponent($('#ingr1').val().trim());
     ingredient2 = encodeURIComponent($('#ingr2').val().trim());
     ingredient3 = encodeURIComponent($('#ingr3').val().trim());
